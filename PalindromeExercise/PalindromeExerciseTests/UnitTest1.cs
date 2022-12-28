9f30fe9317b241be02bc1dac683ab8995ba6d714
Jessica Gonzales <jgonzales0688@gmail.com>
2022-12-27 21:04:21 -0600

    failed tests

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("kayak", true)]
+        [InlineData("redivider",true)]
+        [InlineData("level", true)]
+        public void Test1(string word1,bool expected )
         {
+            var instance = new WordSmith();
 
+            var actual = instance.IsAPalindrome(word1);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
